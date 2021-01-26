7cad2a451a7e29a27988f1f442ef39fdd905122e
Rahul-D-George <rahuld.george@gmail.com>
2021-01-26 14:49:18 +0000

    Had to remove a little bit of problematic code. Pop up layout finished, some basic coding required to get it to show up when challenge button clicked.

---
+++ app/src/main/java/com/example/myapplication/Tapptrial/PopupActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.Tapptrial
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SpinnerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    }
+}
